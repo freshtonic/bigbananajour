bf94822374ea35de8b16bbd3500deb3a6bd6aeb2
James Sadler <freshtonic@gmail.com>
2009-11-13 18:38:28 +1100

    Added bigbananajour.rb and gem_dependencies.rb

---
+++ lib/bigbananajour/gem_dependencies.rb
@@ -0,0 +1,37 @@
+require 'rubygems'
+
+module BigBananajour
+  # DRYs version number dependencies and provides a simple way require them
+  module GemDependencies
+    DEPENDENCIES = [
+      %w( bananajour           2.2.2 ),
+      %w( sinatra              0.9.4 ),
+      %w( json                 1.1.7 ),
+      %w( fancypath            0.5.13 ),
+      %w( rainbow              1.0.1 ),
+      %w( grit                 1.1.1 ),
+      %w( dnssd                1.3.1 ),
+      %w( rack                 1.0.0 ),
+      %w( thin                 1.0.0 ),
+      %w( haml                 2.0.9 ),
+      %w( activesupport        2.3.2 )
+    ]
+    class Dependency < Struct.new(:name, :version)
+      def require_gem; gem name, version end
+    end
+    def self.all
+      DEPENDENCIES.map {|(name, version)| Dependency.new(name, version)}
+    end
+    def self.for_name(name)
+      all.find {|d| d.name == name }
+    end
+  end
+
+  def self.gem(name)
+    BigBananajour::GemDependencies.for_name(name).require_gem
+  end
+  def self.require_gem(name, lib=nil)
+    self.gem(name)
+    Kernel.require(lib || name)
+  end
+end
