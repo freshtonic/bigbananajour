dbae5b50811170424be836ba93e72f5d4650de82
James Sadler <freshtonic@gmail.com>
2009-11-22 10:45:03 +1100

    Capture details of the repo host and store.
    For each cloned repo, capture the person's name and gravatar and stick
    it in the config file of bigbananajour's clone.

--- lib/bigbananajour/bananajour/repository.rb
+++ lib/bigbananajour/bananajour/repository.rb
@@ -5,5 +5,14 @@ module Bananajour
     def uri
       Bananajour.git_uri + path.expand_path.to_s.split("/")[-2..-1].join("/")
     end
+    # TODO: put this in Bananajour.
+    def get_config(key)
+      `cd #{path} && git config --get #{key}`
+    end
+    def set_config(key,value)
+      $stderr.puts "setting '#{key}' to '#{value}'"
+      `cd #{path} && git config --unset-all #{key}`
+      `cd #{path} && git config --replace-all #{key} "#{value}"`
+    end
   end
 end
--- lib/bigbananajour/eater.rb
+++ lib/bigbananajour/eater.rb
@@ -3,22 +3,26 @@ class Bananajour::Eater
     @browser = Bananajour::Bonjour::RepositoryBrowser.new
   end
   def go!
+    $stderr.puts "Starting the EATER!!!"
     while true do
       @browser.repositories.each do |remote_repo|
-        unless remote_repo.ismirror 
-          local_repo = Bananajour::Repository.for_name(make_local_repo_name(remote_repo))
-          if local_repo.exists?
-            $stderr.puts "Eater, fetching changes from remote repo #{remote_repo.name}"
-            fetch_latest(local_repo)
-          else
-            $stderr.puts "Eater, cloning remote repo #{remote_repo.name}"
-            clone_repo(remote_repo)
-            fetch_latest(local_repo)
-          end
+        $stderr.puts "Eater has seen remote repo '#{remote_repo}'"
+        local_repo = Bananajour::Repository.for_name(make_local_repo_name(remote_repo))
+        if local_repo.exists?
+          $stderr.puts "Eater, fetching changes from remote repo #{remote_repo.name}"
+          fetch_latest(local_repo)
+          capture_personal_details(local_repo, remote_repo)
+        else
+          $stderr.puts "Eater, cloning remote repo #{remote_repo.name}"
+          clone_repo(remote_repo)
+          fetch_latest(local_repo)
+          capture_personal_details(local_repo, remote_repo)
         end
       end
+      $stderr.puts "Eater thread SLEEPING"
       sleep 30
     end
+    $stderr.puts "Eater thread exiting."
   end
   private
   def fetch_latest(repo)
@@ -35,10 +39,16 @@ class Bananajour::Eater
     "git clone --mirror #{remote_repo.uri} #{make_local_repo_name(remote_repo)}.git"
     ].join(" && "))
   end
+  # TODO: Re-use the helper version
   def sanitize_email(email)
     email.gsub(/@/, "_at_").gsub(/\./, "_dot_")
   end
   def make_local_repo_name(remote_repo)
     sanitize_email(remote_repo.person.email) + "/" + remote_repo.name
+  end
+  def capture_personal_details(local_repo, remote_repo)
+    $stderr.puts "capturing personal details for #{local_repo.path}"
+    local_repo.set_config("bigbananajour.gravatar", remote_repo.person.gravatar)
+    local_repo.set_config("bigbananajour.person", remote_repo.person.name)
   end
 end
